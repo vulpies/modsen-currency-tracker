c54150a667a3404c91e8c731cb2fe57f68d93e46
vulpies <anonim616@gmail.com>
2023-09-09 20:27:50 +0300

    feat: add contacts page

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Header from './Header'
 import Footer from './Footer'
 import MainCourseInfo from './MainCourseInfo'
 import BanksLocation from './BanksLocation'
+import ContactsInfo from './ContactsInfo'
 
-export { Header, Footer, MainCourseInfo, BanksLocation }
+export { Header, Footer, MainCourseInfo, BanksLocation, ContactsInfo }
